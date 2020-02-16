5f4d7c206eca79c785177b93757097fdf7ac8d25
fdolzanes1 <fdolzanes1@gmail.com>
2020-02-16 13:14:22 -0400

    adding component product

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Header from './components/Header';
+import { FormProduct, ListProduct} from './components/Product';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render () {
+    return (
+      <div className="container">
+        <Header title="Products App"/>
+        <div className="row">
+          <div className="col-md-6"><FormProduct/></div>
+          <div className="col-md-6"><ListProduct/></div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Product/index.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'; 
+import './styles.css';
+
+export class FormProduct extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Cadastro de Produtos</h2>
+      </div>
+    );
+  }
+}
+
+export class ListProduct extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Listar de Produtos</h2>
+      </div>
+    );
+  }
+}
