b6e234ad593a3e68017814a747be059ccf2f85ae
fdolzanes1 <fdolzanes1@gmail.com>
2020-02-16 15:13:34 -0400

    listing contact

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react'; 
+import { Table, Button } from 'reactstrap';
 import './styles.css';
 
 class FormContact extends Component {
   render() {
     return (
       <div>
-        <h2>Cadastro de Contato</h2>
+        
       </div>
     );
   }
@@ -13,10 +14,34 @@ class FormContact extends Component {
 
 class ListContact extends Component {
   render() {
+
+    const { contacts } = this.props; 
+    console.log(contacts);
     return (
-      <div>
-        <h2>Listar de Contato</h2>
-      </div>
+      <Table>
+        <thead>
+          <tr>
+            <th>Nome</th>
+            <th>Email</th>
+            <th>Telefone</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            contacts.map(contact => ( 
+              <tr key={contact._id} >
+                <td>{contact.nome}</td>
+                <td>{contact.email}</td>
+                <td>{contact.telefone}</td>
+                <td><Button color="info" size="sm">Editar</Button>
+                    <Button color="danger" size="sm">Excluir</Button>
+                </td>
+              </tr>  
+            ))
+          }
+        </tbody>
+      </Table>
     );
   }
 }
@@ -32,15 +57,23 @@ export default class ContactBox extends Component {
   componentDidMount() {
     fetch(this.url)
       .then(response => response.json())
-      .then(contacts => console.log(contacts))
+      .then(response => this.setState({ contacts: response.docs }))
       .catch(e => console.log(e));
   }
 
   render() {
     return (
       <div className="row">
-        <div className="col-md-6"><FormContact/></div>
-        <div className="col-md-6"><ListContact/></div>
+        <div className="col-md-6">
+          <h2>Cadastro de Contato</h2>
+          <FormContact/>
+          
+        </div>
+        <div className="col-md-6">
+          <h2>Listar de Contato</h2>
+          <ListContact contacts={this.state.contacts}/>
+          
+        </div>
       </div>
     );
   }
