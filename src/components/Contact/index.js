19dd7ac5817b8f9051d99e1ead6101d5036779cf
fdolzanes1 <fdolzanes1@gmail.com>
2020-02-17 12:40:26 -0400

    adding form create contact

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,13 +1,44 @@
-import React, { Component } from 'react'; 
-import { Table, Button } from 'reactstrap';
+import React, { Component } from 'react';
+import { Table, Button, Form, FormGroup, Label, Input } from 'reactstrap';
 import './styles.css';
 
 class FormContact extends Component {
+
+  state = { 
+    model: {
+      _id: 0,
+      nome: '', 
+      email: '', 
+      telefone: '' 
+    } 
+  };
+
+  setValues = (e, field) => {
+    const { model } = this.state;
+    model[field] = e.target.value;
+    this.setState({ model });
+    console.log(this.state.model);
+  }
+
   render() {
     return (
-      <div>
-        
-      </div>
+      <Form onSubmit={this.enviaForm} method="post">
+        <FormGroup>
+          <Label for="nome">Nome</Label>
+          <Input id="nome" value={this.state.model.nome} type="text" placeholder="Digite o seu Nome" onChange={ e => this.setValues(e, 'nome')}></Input>
+        </FormGroup>
+        <FormGroup>
+          <Label for="email">Email</Label>
+          <Input id="email" value={this.state.model.email} type="text" placeholder="Digite o seu Email" onChange={ e => this.setValues(e, 'email')}></Input>
+        </FormGroup>
+        <FormGroup>
+          <Label for="telefone">Telefone</Label>
+          <Input id="telefone" value={this.state.model.telefone} type="text" placeholder="Digite o seu Telefone" onChange={ e => this.setValues(e, 'telefone')}></Input>
+        </FormGroup>
+        <FormGroup>
+          <Button type="submit" color="primary">Salvar Contato</Button>
+        </FormGroup>
+      </Form>
     );
   }
 }
@@ -15,11 +46,11 @@ class FormContact extends Component {
 class ListContact extends Component {
   render() {
 
-    const { contacts } = this.props; 
+    const { contacts } = this.props;
     console.log(contacts);
     return (
-      <Table>
-        <thead>
+      <Table className="table-bordered text-center">
+        <thead className="thead-dark">
           <tr>
             <th>Nome</th>
             <th>Email</th>
@@ -29,15 +60,15 @@ class ListContact extends Component {
         </thead>
         <tbody>
           {
-            contacts.map(contact => ( 
+            contacts.map(contact => (
               <tr key={contact._id} >
                 <td>{contact.nome}</td>
                 <td>{contact.email}</td>
                 <td>{contact.telefone}</td>
                 <td><Button color="info" size="sm">Editar</Button>
-                    <Button color="danger" size="sm">Excluir</Button>
+                  <Button color="danger" size="sm">Excluir</Button>
                 </td>
-              </tr>  
+              </tr>
             ))
           }
         </tbody>
@@ -66,13 +97,13 @@ export default class ContactBox extends Component {
       <div className="row">
         <div className="col-md-6">
           <h2>Cadastro de Contato</h2>
-          <FormContact/>
-          
+          <FormContact />
+
         </div>
         <div className="col-md-6">
           <h2>Listar de Contato</h2>
-          <ListContact contacts={this.state.contacts}/>
-          
+          <ListContact contacts={this.state.contacts} />
+
         </div>
       </div>
     );
