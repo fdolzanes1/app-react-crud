23a9bf0d17122a93ef5bb9c8366b0beac8e484f2
fdolzanes1 <fdolzanes1@gmail.com>
2020-03-02 19:00:49 -0400

    Updates

---
+++ precache-manifest.ad486a184ae2df87ba5c1ff302d02518.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "662e8d7d19589b1cd2c6b5348732a3cc",
+    "url": "/react-crud/index.html"
+  },
+  {
+    "revision": "169d2bbbb5717f01a1ad",
+    "url": "/react-crud/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "12cc3d80caf6fb7af3d6",
+    "url": "/react-crud/static/css/main.65a8f979.chunk.css"
+  },
+  {
+    "revision": "169d2bbbb5717f01a1ad",
+    "url": "/react-crud/static/js/2.56fe82ef.chunk.js"
+  },
+  {
+    "revision": "bfc08a62983de46a4daec9f8c3c657d8",
+    "url": "/react-crud/static/js/2.56fe82ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12cc3d80caf6fb7af3d6",
+    "url": "/react-crud/static/js/main.4093fc4d.chunk.js"
+  },
+  {
+    "revision": "822b0e953bb7da3b019f",
+    "url": "/react-crud/static/js/runtime-main.0500b41d.js"
+  }
+]);
